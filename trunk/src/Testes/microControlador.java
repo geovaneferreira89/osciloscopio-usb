51d0603ed60a083e97d95982062bfb0b5a1f7d71
vinicius_zaramella@hotmail.com <vinicius_zaramella@hotmail.com@44aad381-89b1-0a71-ad38-706ce7409400>
2011-10-22 02:57:23 +0000

    Criação de classes para simular o hardware. Somente para fim de testes.

---
+++ trunk/src/Testes/microControlador.java
@@ -0,0 +1,12 @@
+package Testes;
+
+public class microControlador {
+		private byte in[];
+		private byte out[];
+		
+		public microControlador(){
+			in = new byte[50];
+			out = new byte[50];
+		}
+		
+}
