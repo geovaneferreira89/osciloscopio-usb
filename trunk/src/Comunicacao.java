2f0070a67788b1b102d611e376aa51e0d4a8cf1e
espuleta0 <espuleta0@44aad381-89b1-0a71-ad38-706ce7409400>
2011-10-26 16:38:03 +0000

    -Alterações na parte de comunicação pela USB

--- trunk/src/Comunicacao.java
+++ trunk/src/Comunicacao.java
@@ -5,6 +5,8 @@ public class Comunicacao implements Runnable{
 	private byte[] input;
 	private byte[] output;
 	private boolean status;	
+	private Device dev = USB.getDevice((short) 0x0483, (short) 0x5750);
+	
 	@Override
 	public void run() {
 		while(true){
@@ -18,11 +20,32 @@ public class Comunicacao implements Runnable{
 	}
 	
 	public byte[] read(){
+	      // data read from the device
+	      byte[] readData = new byte[4];
+	      try 
+	      {
+			dev.open(1, 0, -1);
+			dev.readInterrupt(0x81, readData, readData.length, 2000, false);
+			return readData;
+	      }
+	      catch (USBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
 	}
 
 	public void write(byte [] b){
-		
+	    try 
+	    {
+	        dev.open(1, 0, -1);
+			dev.writeInterrupt(0x81, b, b.length, 2000, false);
+		} 
+	    catch (USBException e)
+	    {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean getStatus(){
